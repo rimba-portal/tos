8e4473e5ec1694c88504f6e7c16258fd35f1e55a
Faros <114036707+a-faros@users.noreply.github.com>
2026-08-01 09:09:59 +0800

    Release v1.0.4

--- src/TosServiceProvider.php
+++ src/TosServiceProvider.php
@@ -6,18 +6,16 @@ namespace Rimba\Tos;
 
 use Rimba\Base\Services\BitesServiceProvider;
 
-
 class TosServiceProvider extends BitesServiceProvider
 {
-
     protected function bootPackage(): void
     {
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         //
     }
+
     protected function registerPackage(): void
     {
         //
     }
-
 }
